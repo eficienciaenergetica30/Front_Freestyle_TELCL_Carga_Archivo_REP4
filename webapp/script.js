c39494383c11ea040bd908b28547e04ecc2c93c4
JorgeFME <jorgemartinez.cloud@gmail.com>
2026-04-28 11:46:34 -0600

    Switch TempElectricFact API to dev host
    Update webapp/script.js to point the axios POST for the TempElectricFact endpoint at the development CAP service host instead of the production host. This routes TempElectricFact data submissions to the dev environment for testing/development.

--- webapp/script.js
+++ webapp/script.js
@@ -132,7 +132,7 @@ async function postSite(data, number) {
     const monthNumber = parseInt(fechaSeleccionada.split("-")[1], 10);
 
     const response = await axios.post(
-      "https://telcl-prd-db-cap-telcl-srv.cfapps.us10.hana.ondemand.com/dataservices/TempElectricFact",
+      "https://telcl-dev-db-cap-telcl-srv.cfapps.us10.hana.ondemand.com/dataservices/TempElectricFact",
       {
         ClRpu: typeof data[0] == "number" ? data[0].toString() : data[0] || "",
         ClTarifa: typeof data[6] == "number" ? data[6].toString() : data[6] || "",
